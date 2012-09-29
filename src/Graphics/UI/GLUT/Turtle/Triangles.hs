8bc6f9f61dfd019ba10ab41bc41646628a9e9edf
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-09-29 13:30:49 +0900

    correct bug of Triangles

--- src/Graphics/UI/GLUT/Turtle/Triangles.hs
+++ src/Graphics/UI/GLUT/Turtle/Triangles.hs
@@ -8,9 +8,9 @@ ps' = [(1, 3), (2.1, 2), (3, 3.1), (5, 2), (4, 1), (2, 1.5), (1, 1.1)]
 main = draw ps' $ toTriangles ps'
 
 toTriangles :: [Pos] -> [(Pos, Pos, Pos)]
-toTriangles ps
-	| isRight tri = toTrianglesTop ps'
-	| otherwise = toTrianglesTop $ reverse ps'
+toTriangles ps = toTrianglesTop ps'
+--	| isRight tri = toTrianglesTop ps'
+--	| otherwise = toTrianglesTop $ reverse ps'
 	where
 	ps' = deleteOnline ps
 	(tri, rest) = distant3 ps
@@ -18,16 +18,16 @@ toTriangles ps
 toTrianglesTop :: [Pos] -> [(Pos, Pos, Pos)]
 toTrianglesTop [a, b, c] = [(a, b, c)]
 toTrianglesTop ps
-	| any (within tri) ps = toTrianglesNext i ps
+	| any (within tri) ps = toTrianglesNext (isRight tri) i ps
 	| otherwise = tri : toTrianglesTop rest
 	where
 	i = far ps
 	tri = index3 ps i
 	rest = deleteIndex ps i
 
-toTrianglesNext :: Int -> [Pos] -> [(Pos, Pos, Pos)]
-toTrianglesNext i ps
-	| any (within tri) ps = toTrianglesNext next ps
+toTrianglesNext :: Bool -> Int -> [Pos] -> [(Pos, Pos, Pos)]
+toTrianglesNext r i ps
+	| isRight tri /= r || any (within tri) ps = toTrianglesNext r next ps
 	| otherwise = tri : toTrianglesTop rest
 	where
 	tri = index3 ps i

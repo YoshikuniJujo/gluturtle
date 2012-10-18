4e5b181450d823afb9d5f7a7cd905a15aa984005
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-10-18 11:19:43 +0900

    separate Fields.hs to Fields.hs and Console.hs

--- src/Graphics/UI/GLUT/Turtle/Field.hs
+++ src/Graphics/UI/GLUT/Turtle/Field.hs
@@ -54,14 +54,16 @@ import Graphics.UI.GLUT.Turtle.Triangles
 
 import qualified Graphics.UI.GLUT.Turtle.GLUTools as G
 import Graphics.UI.GLUT.Turtle.GLUTools(
-	($=), initialize, createWindow, printLines, keyboardCallback, loop,
+	($=), initialize, createWindow, loop,
 	displayAction)
 import Text.XML.YJSVG(Position(..), Color(..))
 
-import Control.Concurrent(ThreadId, forkIO, Chan, newChan, writeChan, readChan)
+import Control.Concurrent(ThreadId, forkIO, readChan)
 import Data.IORef(IORef, newIORef, readIORef, writeIORef)
 import Data.IORef.Tools(atomicModifyIORef_)
 import Data.Maybe
+
+import Graphics.UI.GLUT.Turtle.Console
 
 --------------------------------------------------------------------------------
 
@@ -76,15 +78,6 @@ prompt f p = do
 		_ -> return ()
 
 data Coordinates = CoordTopLeft | CoordCenter
-
-data Console = Console{
-	cPrompt :: IORef String,
-	cCommand :: IORef [String],
-	cHistory :: IORef [String],
-	cChanged :: IORef Int,
-	cChanChanged :: IORef Int,
-	cChan :: Chan String
- }
 
 data Field = Field{
 	fConsole :: IORef (Maybe Console),
@@ -104,59 +97,6 @@ data Field = Field{
  }
 
 --------------------------------------------------------------------------------
-
-openConsole :: String -> Int -> Int -> IO Console
-openConsole name w h = do
-	cwindow <- createWindow name w h
-	cprompt <- newIORef ""
-	ccommand <- newIORef [""]
-	chistory <- newIORef []
-	cchanged <- newIORef 1
-	cchanchanged <- newIORef 0
-	cchan <- newChan
-	let	c = Console{
-			cPrompt = cprompt,
-			cCommand = ccommand,
-			cHistory = chistory,
-			cChanged = cchanged,
-			cChanChanged = cchanchanged,
-			cChan = cchan }
-	keyboardCallback $ processKeyboard c
-	displayAction cchanged $ do
-		cmd <- readIORef ccommand
-		hst <- readIORef chistory
-		printLines cwindow 1.0 $ reverse cmd ++ hst
-	return c
-
-processKeyboard ::
-	Console -> Char -> G.KeyState -> G.Modifiers -> G.Position -> IO ()
-processKeyboard console '\r' G.Down _ _ = do
-	atomicModifyIORef_ (cChanged console) (+ 1)
-	p <- readIORef $ cPrompt console
-	str <- readIORef (cCommand console)
-	atomicModifyIORef_ (cHistory console) (reverse str ++)
-	writeIORef (cCommand console) [p]
-	atomicModifyIORef_ (cChanChanged console) (+ 1)
-	writeChan (cChan console) $ drop (length p) $ concat str
-processKeyboard c '\b' G.Down _ _ = do
-	atomicModifyIORef_ (cChanged c) (+ 1)
-	p <- readIORef $ cPrompt c
-	atomicModifyIORef_ (cCommand c) $ \s -> case s of
-		[""] -> [""]
-		[ss] | length ss <= length p -> s
-		_ -> case (init s, last s) of
-			(i, "") -> init i ++ [init $ last i]
-			(i, l) -> i ++ [init l]
-processKeyboard c chr G.Down _ _ = do
-	atomicModifyIORef_ (cChanged c) (+ 1)
-	atomicModifyIORef_ (cCommand c) (`addToTail` chr)
-processKeyboard _ _ _ _ _ = return ()
-
-addToTail :: [String] -> Char -> [String]
-addToTail [] _ = error "bad"
-addToTail strs c
-	| length (last strs) < 50 = init strs ++ [last strs ++ [c]]
-	| otherwise = strs ++ [[c]]
 
 openField :: String -> Int -> Int -> IO Field
 openField name w h = do
